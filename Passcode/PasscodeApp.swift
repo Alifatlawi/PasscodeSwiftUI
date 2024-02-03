2322de5ba8e4be9ca9352b17112b76ad62bcf301
Ali Al-Fatlawi <alawi_amer@yahoo.co.uk>
2024-02-03 19:52:26 +0300

    Initial Commit

---
+++ Passcode/PasscodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasscodeApp.swift
+//  Passcode
+//
+//  Created by Ali Amer on 2/3/24.
+//
+
+import SwiftUI
+
+@main
+struct PasscodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
